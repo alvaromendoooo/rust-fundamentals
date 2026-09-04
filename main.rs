2b1fffbe9f2c916f952dde932284ba2c1e36288a
alvaromendoooo <alvaromendo2004@gmail.com>
2026-09-04 11:30:08 +0200

    done

--- main.rs
+++ main.rs
@@ -1,3 +1,18 @@
+use std::io::Read;
+
 fn main() {
-    println!("Hello, Rust!");
+
+    // Test 01
+    //println!("Hello, Rust!");
+
+    // Test 02
+    let mut input = String::new();
+    std::io::stdin().read_to_string(&mut input).unwrap();
+    let mut numbers = input.split_whitespace(); // Split the inputs from
+    // whitespace separation
+    let a: i64 = numbers.next().unwrap().parse().unwrap(); // No need explicit parse type because it
+    // takes it from variable definition type
+    let b: i64 = numbers.next().unwrap().parse().unwrap();
+    
+    println!("{}", a + b);
 }
